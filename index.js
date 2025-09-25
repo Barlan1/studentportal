edda4b7194de3af291421fc4b018d1c00cd41322
Vikas Meena <vikasbarlan1@gmail.com>
2025-09-26 04:01:45 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
+import Students from "./Students";
+import StudentDetail from "./StudentDetail";
+function App() {
+  return (
+    <div>
+      <h1>Student Portal</h1>
+      <BrowserRouter>
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
+          <Link to="/students">Students</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/students" element={<Students />} />
+          <Route path="/students/:id" element={<StudentDetail />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<App />);
